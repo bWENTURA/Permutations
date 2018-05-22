e03c18da44afa64c3d2db1839bf6562116fd0a42
bWENTURA <maciej.b.iwanski@gmail.com>
2018-05-22 23:18:10 +0200

    1.0

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <list>
 #include <algorithm>
 
+//Funkcja zwracająca wartość N!
 int factorial(const int N){
-    int temp = 0;
-    for(int i = 0; i < N; ++i)
-        temp *= i + 1;
+    int temp = 1;
+    for(int i = 2; i <= N; ++i)
+        temp *= i;
     return temp;
 }
 
+//Funkcja tworząca vector o zawartości [1, 2, 3, ..., N]
 std::vector<int> create_vector(const int N){
     std::vector<int> array(N);
     for(int i = 0; i < N; ++i)
@@ -17,30 +18,26 @@ std::vector<int> create_vector(const int N){
     return array;
 }
 
-std::list<std::vector<int>> create_array_of_perm(const int N){
+//Funkcja zwracająca tablicę tablic (macierz) permutacji
+std::vector<std::vector<int>> create_array_of_permutations(const int N){
     std::vector<int> temp_array = create_vector(N);
-    std::list<std::vector<int>> matrix;  
+    std::vector<std::vector<int>> matrix;
+    matrix.reserve(factorial(N));
     while(next_permutation(temp_array.begin(), temp_array.end())){
         int j = 0;
         while((j != N - 1) && (temp_array[j] != temp_array[j + 1] + 1) && (temp_array[j] != temp_array[j + 1] - 1))
             ++j;
-        if(j == N - 1) matrix.push_back(temp_array);
+        if(j == N - 1) 
+            matrix.push_back(temp_array);
     }
     return matrix;
 }
 
-
-
 int main(){
     int N;
-    std::list<std::vector<int>> matrix;
+    std::vector<std::vector<int>> matrix;
     std::cout << "Enter limit of the set [1..N]: " << std::endl;
     std::cin >> N;
-    matrix = create_array_of_perm(N);
-    for(std::list<std::vector<int>>::iterator it = matrix.begin() ; it != matrix.end(); ++it){
-        for(auto range_it: *it) std::cout << range_it << " ";
-        std::cout << std::endl;
-    }
-    system("pause");
+    matrix = create_array_of_permutations(N);
     return 0;
 }
